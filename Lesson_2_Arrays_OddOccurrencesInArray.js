e9362d7049a243981a2c8ce3992f825506b91da3
Bruno Medeiros <bruno.am19@gmail.com>
2021-12-27 16:52:31 +0400

    OddOccurrencesInArray refactored

--- Lesson_2_Arrays_OddOccurrencesInArray.js
+++ Lesson_2_Arrays_OddOccurrencesInArray.js
@@ -7,16 +7,34 @@
  * @return {array} the value of the unpaired element
  */
 const solution = (A) => {
-  return A.reduce((previousValue, currentValue) => {
-    console.log(previousValue, currentValue);
-    console.log(previousValue.indexOf(currentValue));
+  const set = new Set([]);
 
-    if (previousValue.indexOf(currentValue) === -1) {
-      previousValue.push(currentValue);
+  for (const item of A) {
+    if (set.has(item)) {
+      set.delete(item);
+    } else {
+      set.add(item);
     }
+  }
 
-    return previousValue;
-  }, []);
+  return Array.from(set)[0];
 };
 
+// Other solution:
+// const solution = (A) => {
+//   let result = 0;
+
+//   for (let element of A) {
+//     result ^= element;
+//   }
+
+//   return result;
+// };
+
 console.log(solution([9, 3, 9, 3, 9, 7, 9])); // 7
+console.log(solution([1, 2, 1, 3, 5, 2, 3])); // 5
+console.log(solution([1, 2, 1, 3, 5, 2, 3, 1, 1, 2, 2])); // 5
+console.log(solution([42])); // 42
+console.log(
+  solution([1000000, 2000000, 1000000, 30000000, 5000000, 2000000, 30000000])
+); // 5000000
